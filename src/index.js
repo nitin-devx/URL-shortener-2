4ea9b18c529a3cd41479d6f72cd7402ffbeff661
nitin-devx <nitin.5316401524@std.ggsipu.ac.in>
2026-03-23 21:47:12 +0530

    mongoDb connection

--- src/index.js
+++ src/index.js
@@ -3,7 +3,20 @@ import dotenv from "dotenv";
 dotenv.config({ path: "./.env" });
 
 import connectDB from "./db/index.js";
+import {app} from "./app.js";
 
 
 
-connectDB();
+connectDB()/// ITS store a promise
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`server is running on port ${process.env.PORT || 8000}`);
+    });
+    app.on("error",(err)=>{
+        console.log("error",err);
+        throw err;
+    })
+})
+.catch((err)=>{
+    console.log("MONGO DB CONNECTION FAILED",err)
+})
