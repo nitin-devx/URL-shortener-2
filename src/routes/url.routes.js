893ce8ddda74215f4a554703efb2bb06b98b0a69
Nitin <nitin.5316401524@std.ggsipu.ac.in>
2026-03-25 21:33:27 +0530

    Fix import statement for url.shortId.js

--- src/routes/url.routes.js
+++ src/routes/url.routes.js
@@ -1,6 +1,6 @@
 import { Router }  from "express";
 import urlShorten from "../controllers/url.shorten.js";
-import urlshortId from "../controllers/url.shortid.js";
+import urlshortId from "../controllers/url.shortId.js";
 import urlstatsShortId from "../controllers/url.statsshortId.js";
 
 const router = Router();
